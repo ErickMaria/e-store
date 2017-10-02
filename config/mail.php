e8afc923ba69eb8a60373760ed125d1ab3f8ab05
erickmrds0 <erickmrds0@gmail.com>
2017-10-02 02:13:33 -0400

    start of password recovery function

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'erickmrds0@gmail.com', 'name' => 'E-store'],
 
     /*
     |--------------------------------------------------------------------------
